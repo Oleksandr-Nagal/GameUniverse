651d09b8f994de3d82ee83bb2ea2566514e05341
Oleksandr Nagal <vt221_nos@student.ztu.edu.ua>
2025-02-06 11:06:13 +0200

    Додав контролери та базові сторінки для Каталогу, Про нас і Авторизації

---
+++ GameUniverse/Controllers/CatalogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameUniverse.Controllers
+{
+    public class CatalogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
